466bb0dc26c3dae2ad77338905c9500bedd2ffca
Seonggi Yang <seonggi.yang@gmail.com>
2017-07-11 22:02:51 +0800

    Foo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sysctl -w net.ipv4.conf.all.forwarding=1
+sysctl -w net.ipv6.conf.all.forwarding=1
+
 cat > /etc/ppp/peers/vpn <<_EOF_
 pty "pptp ${SERVER} --nolaunchpppd"
 lock
@@ -26,6 +29,9 @@ cat > /etc/ppp/ip-down <<_EOF_
 ip route del 0.0.0.0/1 dev ppp0
 ip route del 128.0.0.0/1 dev ppp0
 _EOF_
+
+# fooo
+route add -net 192.168.0.0 netmask 255.255.0.0 gw 172.17.0.1
 
 pon vpn debug dump logfd 2 updetach persist "$@"
 exec sockd
